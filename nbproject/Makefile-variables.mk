d26ca92e2563f6c1a7ff7c1cae0ebe4b1ce04b40
vtrevino <vtrevino@users.noreply.github.com>
2025-06-24 09:40:47 -0600

    Add files via upload

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,75 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=CLang-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/CLang-MacOSX
+CND_ARTIFACT_NAME_Debug=ogmapper
+CND_ARTIFACT_PATH_Debug=dist/Debug/CLang-MacOSX/ogmapper
+CND_PACKAGE_DIR_Debug=dist/Debug/CLang-MacOSX/package
+CND_PACKAGE_NAME_Debug=ogmapper.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/CLang-MacOSX/package/ogmapper.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=ogmapper
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/ogmapper
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=ogmapper.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/ogmapper.tar
+# Release_Intel_CLANG configuration
+CND_PLATFORM_Release_Intel_CLANG=CLang-MacOSX
+CND_ARTIFACT_DIR_Release_Intel_CLANG=dist/Release_Intel_CLANG/CLang-MacOSX
+CND_ARTIFACT_NAME_Release_Intel_CLANG=ogmapper
+CND_ARTIFACT_PATH_Release_Intel_CLANG=dist/Release_Intel_CLANG/CLang-MacOSX/ogmapper
+CND_PACKAGE_DIR_Release_Intel_CLANG=dist/Release_Intel_CLANG/CLang-MacOSX/package
+CND_PACKAGE_NAME_Release_Intel_CLANG=ogmapper.tar
+CND_PACKAGE_PATH_Release_Intel_CLANG=dist/Release_Intel_CLANG/CLang-MacOSX/package/ogmapper.tar
+# Release_M_CLANG configuration
+CND_PLATFORM_Release_M_CLANG=CLang-MacOSX
+CND_ARTIFACT_DIR_Release_M_CLANG=dist/Release_M_CLANG/CLang-MacOSX
+CND_ARTIFACT_NAME_Release_M_CLANG=ogmapper
+CND_ARTIFACT_PATH_Release_M_CLANG=dist/Release_M_CLANG/CLang-MacOSX/ogmapper
+CND_PACKAGE_DIR_Release_M_CLANG=dist/Release_M_CLANG/CLang-MacOSX/package
+CND_PACKAGE_NAME_Release_M_CLANG=ogmapper.tar
+CND_PACKAGE_PATH_Release_M_CLANG=dist/Release_M_CLANG/CLang-MacOSX/package/ogmapper.tar
+# Release_Win_CLANG configuration
+CND_PLATFORM_Release_Win_CLANG=CLang-MacOSX
+CND_ARTIFACT_DIR_Release_Win_CLANG=dist/Release_Win_CLANG/CLang-MacOSX
+CND_ARTIFACT_NAME_Release_Win_CLANG=ogmapper
+CND_ARTIFACT_PATH_Release_Win_CLANG=dist/Release_Win_CLANG/CLang-MacOSX/ogmapper
+CND_PACKAGE_DIR_Release_Win_CLANG=dist/Release_Win_CLANG/CLang-MacOSX/package
+CND_PACKAGE_NAME_Release_Win_CLANG=ogmapper.tar
+CND_PACKAGE_PATH_Release_Win_CLANG=dist/Release_Win_CLANG/CLang-MacOSX/package/ogmapper.tar
+# Release_Native_GNU configuration
+CND_PLATFORM_Release_Native_GNU=GNU-MacOSX
+CND_ARTIFACT_DIR_Release_Native_GNU=dist/Release_Native_GNU/GNU-MacOSX
+CND_ARTIFACT_NAME_Release_Native_GNU=ogmapper
+CND_ARTIFACT_PATH_Release_Native_GNU=dist/Release_Native_GNU/GNU-MacOSX/ogmapper
+CND_PACKAGE_DIR_Release_Native_GNU=dist/Release_Native_GNU/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release_Native_GNU=ogmapper.tar
+CND_PACKAGE_PATH_Release_Native_GNU=dist/Release_Native_GNU/GNU-MacOSX/package/ogmapper.tar
+# Release_Linux_CLANG configuration
+CND_PLATFORM_Release_Linux_CLANG=CLang-MacOSX
+CND_ARTIFACT_DIR_Release_Linux_CLANG=dist/Release_Linux_CLANG/CLang-MacOSX
+CND_ARTIFACT_NAME_Release_Linux_CLANG=ogmapper
+CND_ARTIFACT_PATH_Release_Linux_CLANG=dist/Release_Linux_CLANG/CLang-MacOSX/ogmapper
+CND_PACKAGE_DIR_Release_Linux_CLANG=dist/Release_Linux_CLANG/CLang-MacOSX/package
+CND_PACKAGE_NAME_Release_Linux_CLANG=ogmapper.tar
+CND_PACKAGE_PATH_Release_Linux_CLANG=dist/Release_Linux_CLANG/CLang-MacOSX/package/ogmapper.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
---
+++ nbproject/private/Makefile-variables.mk
@@ -0,0 +1,12 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+# Debug configuration
+# Release configuration
+# Release_Intel_CLANG configuration
+# Release_M_CLANG configuration
+# Release_Win_CLANG configuration
+# Release_Native_GNU configuration
+# Release_Linux_CLANG configuration
