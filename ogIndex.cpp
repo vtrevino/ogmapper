5cc8dda5a7a5b4902ea1bfab265614cdd5a75b5a
vtrevino <vtrevino@users.noreply.github.com>
2026-09-12 18:08:50 -0600

    New mapping message format

--- ogIndex.cpp
+++ ogIndex.cpp
@@ -1721,9 +1721,9 @@ void ogIndex::mapOrCount(char *pSourceFileName1, char *pSourceFileName2, char mo
     //    caReads->push(new ogSingleRead());
     //}
 
-    fprintf(stderr, "/--------------------------------------\\\n");
-    fprintf(stderr, "|1 Million Reads: .=25,000 %2u+1 threads|", nThreads); fflush(stderr); // mode == 'M' ? "mapping" : "counting"
-    const char *TITLE = "|--------------------------------------|Reads/s|Mreads|Elap t|Tot r/s|%Done|Left t|";
+    fprintf(stderr, "/=========== 1 Million Reads ===========\\\n");
+    fprintf(stderr, "| .=25,000 Reads           %2u+1 threads |", nThreads); fflush(stderr); // mode == 'M' ? "mapping" : "counting"
+    const char *TITLE = "|---------------------------------------|Reads/s|Mreads|Elap t|Tot r/s|%Done|Left t|";
     //fprintf(stderr, "%s", TITLE);fflush(stderr); // Map %%|
     if (nThreads > 0) {
         hilos = (thread **) malloc(nThreads * sizeof(thread *));
@@ -1875,7 +1875,7 @@ void ogIndex::mapOrCount(char *pSourceFileName1, char *pSourceFileName2, char mo
                     uint64_t blockElapsed = chrono::duration_cast<chrono::milliseconds>(end - block).count();
                     if (r > 0) {
                         float fp = (float) curElapsed / 1000.0;
-                        fprintf(stderr, "%7d|%6.0f|%3d:%02d|%7d|", 
+                        fprintf(stderr, "|%7d|%6.0f|%3d:%02d|%7d|", 
                                 (int) ((float) 1000000.0 / (float) (blockElapsed / 1000.0)), 
                                 (double) r / 1000000.0, 
                                 (int) trunc(fp / 60), (int) (fp - 60* trunc((fp / 60.0))), ///(curElapsed / 1000), 
@@ -1894,7 +1894,7 @@ void ogIndex::mapOrCount(char *pSourceFileName1, char *pSourceFileName2, char mo
                         //for (int ith=0; ith <= nThreads; ith++) nUnMap += rdMapr[ith]->nReadsUnmapped;
                         //fprintf(stderr, "%4.1f%%|", (float) (100.0- ((float) nUnMap*100.0)/(float) rAssigned));
                     }
-                    if (r % 50000000 == 0) fprintf(stderr, "\n%s\n", TITLE); else fprintf(stderr, "\n");
+                    if (r % 30000000 == 0) fprintf(stderr, "\n%s\n", TITLE); else fprintf(stderr, "\n");
                     fflush(stderr);
                     block = end;
                 }
